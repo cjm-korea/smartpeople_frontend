7b6bf2bba6f071550bd3158085a8daac2b0e00a3
CJM <whdals9514@naver.com>
2023-04-06 21:13:32 +0900

    ui

--- components/header.js
+++ components/header.js
@@ -1,10 +1,14 @@
 import { useState } from "react"
+import { FaMoneyCheck } from "react-icons/fa";
+import { FiLogIn } from  'react-icons/fi'
+import { GiBullseye } from "react-icons/gi";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 export default function Header() {
-    const [navbarOpen, setNavbarOpen] = useState(false);
-    return (
-        <>
-            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-[#f5f5f7] mb-3">
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-[#f5f5f7] mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-black"
@@ -14,46 +18,32 @@ export default function Header() {
 
             <button
               className='lg:hidden block text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent outline-none focus:outline-none'
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              메뉴
-              <i className="fas fa-bars"></i>
+              type="button" onClick={() => setNavbarOpen(!navbarOpen)}>
+              <HiMenuAlt3 size="36" color="#000000" className="mr-2" />
             </button>
 
           </div>
-          <div className={ `${navbarOpen ? 'flex' : 'hidden'} lg:flex flex-grow items-center text-black`}
-            id="example-navbar-danger"
-          >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                  href="#"
-                >
-                  <i className="fab fa-facebook-square text-lg leading-lg opacity-75"></i><span className="ml-2">소개</span>
+          <div className={`${navbarOpen ? 'flex' : 'hidden'} lg:flex flex-grow items-center justify-end text-black`} id="example-navbar-danger">
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto text-lg leading-lg">
+              <li className="nav-item flex justify-center">
+                <a className="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75" href="#">
+                <GiBullseye size="27" color="#D32F2F" className="mr-2" /> 소개
                 </a>
               </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                  href="#"
-                >
-                  <i className="fab fa-twitter text-lg leading-lgopacity-75"></i><span className="ml-2">요금제</span>
+              <li className="nav-item flex justify-center">
+                <a className="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75" href="#">
+                <FaMoneyCheck size="27" color="#51abf3" className="mr-2" /> 요금제
                 </a>
               </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug  hover:opacity-75"
-                  href="#"
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg opacity-75"></i><span className="ml-2">로그인</span>
+              <li className="nav-item flex justify-center">
+                <a className="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug  hover:opacity-75" href="#">
+                <FiLogIn size="27" color="#000000" className="mr-2" /> 로그인
                 </a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-        </>
-    )
+    </>
+  )
 }
