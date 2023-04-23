5174bb26da6300eaca7a7c8eab6e5fb0d88b2a52
CJM <whdals9514@naver.com>
2023-04-23 16:00:51 +0900

    ssr

--- components/data/columns.js
+++ components/data/columns.js
@@ -1,5 +1,4 @@
 export const columns = [
-    // { field: 'id', headerName: 'ID', width: 150 },
     { field: 'userName', headerName: '이름', width: 200 },
     { field: 'myNumber', headerName: '번호', width: 250 },
     { field: 'parentNumber', headerName: '학부모 번호', width: 250 }
