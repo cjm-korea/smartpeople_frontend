b04a49c104410e4525219530c5b80315a20eb0a1
CJM <whdals9514@naver.com>
2023-05-13 17:31:10 +0900

    ui update

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
       </Head>
 
       <section className='relative w-screen h-[50vh]'>
-        <h1 className='absolute top-4 left-0 right-0 z-10 text-center leading-relaxed p-0 m-0 text-white text-[4rem]'>
+        <h1 className='absolute w-full h-fit top-12 bottom-0 left-0 right-0 z-10 text-center leading-relaxed p-0 m-0 text-white text-[3rem]'>
           차량 및 출결 연동
           <br />
           학생 관리 서비스
@@ -85,10 +85,19 @@ export default function Home() {
               priority='true'
             />
           </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          ...
+          <SwiperSlide>
+            <Image
+              src="/class_02.png"
+              width={'0'}
+              height={'0'}
+              sizes='100vw'
+              alt='main_img_1'
+              className='w-full h-full'
+              quality={100}
+              priority='true'
+            />
+          </SwiperSlide>
+          {/* <SwiperSlide>Slide 3</SwiperSlide> */}
         </Swiper>
       </section>
 
@@ -98,8 +107,11 @@ export default function Home() {
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">효율적인 학원 운영의 길
               {/* <br class="hidden lg:inline-block">readymade gluten/</br> */}
             </h1>
-            <p className="mb-8 leading-relaxed">아이의 학원 스케쥴이나 수업에 잘 참석하고 있는지에 대한 여부를 궁금해하는 학부모가 많으나 학원의 관리 인력 및 시스템 부족으로 아이들의 정보를 실시간으로 알려주지 못합니다.
-              학원에는 근무자 간 체계적인 업무 관리 채널이 없습니다. 근무자 간 체계적인 업무 관리 채널이 없기 때문에 불편사항에 대한 빠른 피드백과 업무 효용이 잘 일어나고 있지 않습니다.</p>
+            <p className="mb-8 leading-relaxed">
+              1. 아이의 출결 상황을 알고 싶나요?<br/>
+              2. 실시간으로 통학 버스의 위치를 알고 싶나요?<br/>
+              3. 이 모든 서비스를 초기비용 없이 이용하고 싶나요?
+            </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">연락하기</button>
             </div>
@@ -115,28 +127,30 @@ export default function Home() {
           <div className="flex flex-col text-center w-full mb-20">
             <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">학원톡톡만의 서비스</h2>
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">학원 운영 종합 패키지</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">기존방식에서 벗어나 학원 운영관리가 아닌 소비자들이 원하는 방식으로 해결방법</p>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              진짜 이용자가 원하는 방식으로!
+            </p>
           </div>
           <div className="flex flex-wrap">
             <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
-              <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-              <Link href='/' className="text-indigo-500 inline-flex items-center">Learn More
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">등하원 알림 서비스</h2>
+              <p className="leading-relaxed text-base mb-4">초기비용 없이 학생의 등하원 알림 서비스를 학부모에게 보낼 수 있습니다.</p>
+              <Link href='/' className="text-indigo-500 inline-flex items-center">더보기
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
             </div>
             <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">The Catalyzer</h2>
-              <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-              <Link href='/' className="text-indigo-500 inline-flex items-center">Learn More
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">실시간 통학버스 위치보기</h2>
+              <p className="leading-relaxed text-base mb-4">학원의 통학버스 위치를 실시간으로 보여주게 됩니다.</p>
+              <Link href='/' className="text-indigo-500 inline-flex items-center">서비스 준비중
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            {/* <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
               <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Neptune</h2>
               <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
               <Link href='/' className="text-indigo-500 inline-flex items-center">Learn More
@@ -153,12 +167,11 @@ export default function Home() {
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
-            </div>
+            </div> */}
           </div>
-          <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">자세히 보기</button>
         </div>
       </section>
-
     </Layout>
   )
 }
